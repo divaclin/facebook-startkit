0c23b470135618c3b4ef2ff4c4cdf1c63ae1c3b2
divac lin <divac.d.zoro@gmail.com>
2015-06-03 13:46:16 +0800

    dfdf

--- js/graphApi.js
+++ js/graphApi.js
@@ -11,6 +11,6 @@ function loginView(){
 	$('article').empty();
 	$('article').append('<form>username:<id="username" input type="text">password:<id="password" input type="password"><input id="loginBtn" class="btn btn-success" type="submit">login<input id="signupBtn" class="btn btn-primary" type="submit">signup<input id="fbloginBtn" class="btn btn-primary" type="submit">login with facebook</form>');
 }
-function (){
+function indexView(){
 	
 }
