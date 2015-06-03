c793fc810466a30277e742141b42043669bcd398
divac lin <divac.d.zoro@gmail.com>
2015-06-03 19:35:27 +0800

    ASDSD

--- js/graphApi.js
+++ js/graphApi.js
@@ -3,15 +3,6 @@ $(document).ready(function(){
 	console.log(current);
 	if(current){
 	    indexView();
-		FB.getLoginStatus(function(response) {
-		     if (response.status === 'connected') {
-		         uid = response.authResponse.userID;
-		         accessToken = response.authResponse.accessToken;
-		         FB.api('/me/picture?type=large', function (response) {
-				    $('#fbImgView').html("<h5>Here are your profile photo</h5><img src="+response.data.url+" crossorigin=\"anonymous\" id=preview1 />");          
-		         });
-		  }
-	     }); 
 	}
 	else{
 		loginView();
