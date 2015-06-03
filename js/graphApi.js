354e9a08a27128f0163ec30dba4f203a4b7d654c
divac lin <divac.d.zoro@gmail.com>
2015-06-03 13:50:01 +0800

    go

--- js/graphApi.js
+++ js/graphApi.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 });
 function loginView(){
 	$('article').empty();
-	$('article').append('<form>username:<id="username" input type="text">password:<id="password" input type="password"><input id="loginBtn" class="btn btn-success" type="submit">login<input id="signupBtn" class="btn btn-primary" type="submit">signup<input id="fbloginBtn" class="btn btn-primary" type="submit">login with facebook</form>');
+	$('article').append('<form><label for="username">username:</label><id="username" input type="text"><label for="password">password:</label><id="password" input type="password"><input id="loginBtn" class="btn btn-success" type="submit">login<input id="signupBtn" class="btn btn-primary" type="submit">signup<input id="fbloginBtn" class="btn btn-primary" type="submit">login with facebook</form>');
 }
 function indexView(){
 	
