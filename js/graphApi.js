bd521f8985ae70d4f828fe43191972cecd2e9f30
divac lin <divac.d.zoro@gmail.com>
2015-06-03 13:30:31 +0800

    go

---
+++ js/graphApi.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	var current = Parse.User.current();
+	if(current){
+	
+	}
+	else{
+		loginView();
+	}
+}
+function loginView(){
+	$('article').fadeOut();
+}
+function (){
+	
+}
