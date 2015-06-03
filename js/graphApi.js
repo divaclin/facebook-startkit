7c73a2781148e99e5f6814ad5c27af4e6d89120d
divac lin <divac.d.zoro@gmail.com>
2015-06-03 14:09:08 +0800

    adsadsds

--- js/graphApi.js
+++ js/graphApi.js
@@ -4,12 +4,11 @@ $(document).ready(function(){
 	
 	}
 	else{
-		//loginView();
+		loginView();
 	}
 });
 function loginView(){
-	$('article').empty();
-	$('article').append('<form><label>username:<id="username" input type="text"></label><label>password:<id="password" input type="password"></label><button id="loginBtn" class="btn btn-success" type="submit">login</button><button id="signupBtn" class="btn btn-primary" type="submit">signUp</button><button id="fbloginBtn" class="btn btn-primary" type="submit">FBlogin</button></form>');
+	$('#loginView').show();
 }
 function indexView(){
 	
