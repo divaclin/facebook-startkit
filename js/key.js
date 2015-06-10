e6aa719c796f63c6b26ca3998c62246b860cd578
divaclin <divac.d.zoro@gmail.com>
2015-06-10 19:43:19 +0800

    init

--- js/key.js
+++ js/key.js
@@ -1,6 +1,20 @@
 /*請貼上fackbook初始化的相關程式碼*/
 /*https://developers.facebook.com/*/
+ window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '838463579536966',
+      xfbml      : true,
+      version    : 'v2.3'
+    });
+  };
 
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
 
 
 
